4fefde49ab8c3ff9c75e828d908dbead0f033575
LukaszSkraba01 <leventura@protonmail.com>
2023-04-22 23:08:45 +0200

    Filtrowanie pracowników - frontend

--- client/src/pages/pracownicy/Pracownicy.jsx
+++ client/src/pages/pracownicy/Pracownicy.jsx
@@ -7,6 +7,12 @@ const Pracownicy = () => {
   const [employeeList, setEmployeeList] = useState([]);
   const [employeeSites, setEmployeeSites] = useState(1);
   const [employeeNumber, setEmployeeNumber] = useState(0);
+  const [filterFirstName, setFilterFirstName] = useState("");
+  const [filterLastName, setFilterLastName] = useState("");
+  const [filterMinRate, setFilterMinRate] = useState("");
+  const [filterMaxRate, setFilterMaxRate] = useState("");
+  const [filterContractType, setFilterContractType] = useState("");
+  const [filterContractDate, setFilterContractDate] = useState("");
 
   useEffect(() => {
     countEmployees();
@@ -39,20 +45,31 @@ const Pracownicy = () => {
 
   useEffect(() => {
     getEmployees();
-  }, [employeeSites]);
+  }, [employeeSites, filterFirstName, filterLastName, filterMinRate, filterMaxRate, filterContractType, filterContractDate]);
 
   const countEmployees = () => {
     axios.get("http://localhost:3001/employees/").then((response) => {
       setEmployeeNumber(response.data[0].user_count);
     });
   };
-
+  /*
   const getEmployees = () => {
     axios
       .get(`http://localhost:3001/employees/${employeeSites}`)
       .then((response) => {
         setEmployeeList(response.data);
       });
+  };
+  */
+  const getEmployees = () => {
+    const url = `http://localhost:3001/employees/${employeeSites}?firstName=${filterFirstName}&lastName=${filterLastName}&minRate=${filterMinRate}&maxRate=${filterMaxRate}&contractType=${filterContractType}&contractDate=${filterContractDate}`;
+    axios.get(url).then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  const handleFilterChange = (event, setterFunction) => {
+    setterFunction(event.target.value);
   };
 
   return (
@@ -63,6 +80,39 @@ const Pracownicy = () => {
         <Link to="/pracownicy/usun">Usuń pracownika</Link>
         <Link to="/pracownicy/update">Aktualizuj dane</Link>
         <Link to="/pracownicy/contracts">Kontrakty</Link>
+      </div>
+      <div className="pracownicy__filters">
+        <div className="pracownicy__filter">
+        <label htmlFor="filterFirstName">Imię:</label>
+          <input type="text" id="filterFirstName" value={filterFirstName} onChange={(event) => handleFilterChange(event, setFilterFirstName)} />
+        </div>
+        <div className="pracownicy__filter">
+          <label htmlFor="filterLastName">Nazwisko:</label>
+          <input type="text" id="filterLastName" value={filterLastName} onChange={(event) => handleFilterChange(event, setFilterLastName)} />
+        </div>
+        <div className="pracownicy__filter">
+          <label htmlFor="filterMinRate">Minimalna stawka:</label>
+          <input type="number" id="filterMinRate" value={filterMinRate} onChange={(event) => handleFilterChange(event, setFilterMinRate)} />
+        </div>
+        <div className="pracownicy__filter">
+          <label htmlFor="filterMaxRate">Maksymalna stawka:</label>
+          <input type="number" id="filterMaxRate" value={filterMaxRate} onChange={(event) => handleFilterChange(event, setFilterMaxRate)} />
+        </div>
+        <div className="pracownicy__filter">
+          <label htmlFor="filterContractType">Typ kontraktu:</label>
+          <select id="filterContractType" value={filterContractType} onChange={(event) => handleFilterChange(event, setFilterContractType)}>
+            <option value="">-</option>
+            <option value="Permanent">Permanent</option>
+            <option value="Fixed">Fixed time</option>
+            <option value="Fixed">Temporary</option>
+            {/* Dodaj kolejne opcje */}
+          </select>
+        </div>
+        <div className="pracownicy__filter">
+          <label htmlFor="filterContractDate">Data zawarcia kontraktu:</label>
+          <input type="date" id="filterContractDate" value={filterContractDate} onChange={(event) => handleFilterChange(event, setFilterContractDate)} />
+        </div>
+        <button className="pracownicy__filter-button" onClick={getEmployees}>Filtruj</button>
       </div>
       <div className="pracownicy__list">
         <table style={{ width: "100%" }}>
